0be4d85ee7b744bceb00574a95a0df7b51bcca4e
MarinaraRubenichF <mrfumagalli@inf.ufsm.br>
2018-09-03 20:49:14 -0300

    Add files via upload

---
+++ Códigos/exemploFuncao.js
@@ -0,0 +1,7 @@
+var x = minhaFuncaoMultiplicadora(4, 3);    // Function is called, return value will end up in x
+
+function minhaFuncaoMultiplicadora(a, b) {
+    return a * b;            // Function returns the product of a and b
+}
+
+console.log("A multiplicação é: "+x);
